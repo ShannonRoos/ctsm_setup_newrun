857bfaba641151025d981c0e9c58fc19f7f623a9
vsc10951 vscuser <vsc10951@node5409.dodrio.os>
2025-12-01 16:18:06 +0100

    add local folders and files

---
+++ newcase/setup_CTSM52_HS_reprpool.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+
+
+# bash script to set up a CTSMv5.2 transient historical run
+# information taken from Wim Thiery, adopted from:
+# https://wiki.iac.ethz.ch/Climphys/ProjectCESM122SetupControlRun
+
+
+
+#=====================================================
+# initialisation
+#=====================================================
+
+
+# define path where cases are stored
+CASEDIR=~/cases
+
+
+# define path where CESM scripts are stored
+SCRIPTSDIR=~/code/CTSM_HS/cime/scripts
+
+# define output directory
+OUTDIR=~/scratch
+
+# define machine
+MACH=derecho
+
+
+# define project
+PROJ=P93300041
+
+
+# define compiler
+#COMPILER=intel;  MPILIB=openmpi      # io
+
+
+# define compset - more information below
+COMP=IHistClm50BgcCrop
+
+
+# define resolution
+RES=f19_g17          # 1.9x2.5_gx1v7 (2°) resolution
+
+
+# define number of cores
+NTASKS=128           # number of tasks (= cores used)
+
+
+# define other information
+ver=e52                                                       # version, used in CASE name, e122 = CESM 1.2.2
+#flavor="${COMPILER:0:1}${MPILIB:0:1}"                        # either im, io, pm, po
+type=control                                                  # type of run
+desc=HSF_repr_func_edits                            # description, used in CASE name
+nnn=3                                                  # unique number, used in CASE name
+comp=$( echo ${COMP:0:5} | tr '[:upper:]' '[:lower:]' )
+
+
+# define run settings
+start_year=1980 # start year of the simulation (1976)
+end_year=2014   # end year of the SST dataset
+simul_length=5  # years
+
+
+# define namelist script
+nl_file=user_nl_${nnn}.sh
+
+
+# set whether you are in production mode or test mode
+production=true  # true= production runs; false=testing
+nresubmit=6     # if simul_length=12, this is number of years -1; use for final production runs including spinup
+
+
+
+#=====================================================
+# 1. create new case
+#=====================================================a
+
+
+
+# get path where setup scripts are
+SETUPDIR=$(pwd)
+
+
+# Change into the new case directory 
+cd $SCRIPTSDIR
+
+
+# generate casename
+CASE=$comp.$ver.$COMP.$RES.$desc
+
+
+# create a new case in the directory 'cases'
+./create_newcase --case $CASEDIR/$CASE  --res $RES --compset $COMP --machine $MACH --project $PROJ
+
+##copy CROPHEATSTRESS script to SourceMods
+#cp /glade/u/home/sroos/setup_newrun/HSF_experiments/CropHeatStress_${desc}.F90 $CASEDIR/$CASE/SourceMods/src.clm/CropHeatStress.F90
+
+
+#=====================================================
+# 2. invoke cesm_setup
+#=====================================================
+
+
+# Change into the new case directory 
+cd $CASEDIR/$CASE
+
+
+# copy this script to the case directory for reference
+cp $SETUPDIR/`basename "$0"` .
+
+# Modify env_build.xml
+./xmlchange CIME_OUTPUT_ROOT=$OUTDIR
+
+# Modify env_mach_pes.xml
+# Note: If you change env_mach_pes.xml later, you have to do ./cesm_setup -clean; and again ./cesm_setup; ./$CASE.$MACH.build ! 
+./xmlchange NTASKS=$NTASKS
+
+# modify env_run.xml
+./xmlchange DOUT_S=FALSE			# short-time archiving
+./xmlchange RUN_STARTDATE=${start_year}"-01-01"  # year in which CESM starts running (1st January)
+./xmlchange DATM_YR_ALIGN=${start_year}          # model year when you want the data to start
+./xmlchange DATM_YR_START=${start_year}          # date on the file when the data starts
+./xmlchange DATM_YR_END=${end_year}            # date on the file when the data ends
+./xmlchange RUN_TYPE=startup                # Set to run type to startup (is the default)
+
+
+# modify env_run.xml - MAY BE CHANGED ANYTIME during a run
+./xmlchange STOP_OPTION=nyears
+### ./xmlchange -file env_run.xml -id STOP_OPTION       -val ndays
+./xmlchange STOP_N=${simul_length}
+
+
+
+# introduce changes to CLM namelist
+cp $SETUPDIR/$nl_file .
+chmod 750 $nl_file
+./$nl_file
+
+
+# Configure case
+./case.setup
+
+
+
+#=====================================================
+# 3. Build/Compile the model
+#    note: restart from here when making source mods
+#=====================================================
+
+
+# Build/Compile the model
+./case.build
+
+
+
+#=====================================================
+# 4. Submit run to the batch queue
+#=====================================================
+
+
+# change walltime from 23:59h to 03:59h - for 2degree run
+### sed -i 's/#BSUB -W 23:59/#BSUB -W 03:59/g' $CASE.run
+
+
+# run the model
+if [ "$production" = true ]; then  # production runs
+
+  ./xmlchange CONTINUE_RUN=FALSE
+  ./xmlchange RESUBMIT=${nresubmit}
+  ./case.submit
+
+ 
+else                               # single year run
+
+  ./case.submit
+
+fi
+
+
+# Check the submitted job status (can take a bit before status has come through)
+#qstat -u sroos
+
+
+
+
+
---
+++ newcase/user_nl.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+# Author: Shannon de Roos (shannon.de.roos@vub.be), structure adopted from Wim Thiery.
+# created: 8/11/24
+
+# use this file to changes namelist settings for associated run
+
+# CLM5
+# add history fields, only output relevant variables
+cat > user_nl_clm << EOF
+!----------------------------------------------------------------------------------
+! Users should add all user specific namelist changes below in the form of
+! namelist_var = new_namelist_value
+!
+! EXCEPTIONS:
+! Set use_cndv           by the compset you use and the CLM_BLDNML_OPTS -dynamic_vegetation setting
+! Set use_vichydro       by the compset you use and the CLM_BLDNML_OPTS -vichydro           setting
+! Set use_cn             by the compset you use and CLM_BLDNML_OPTS -bgc  setting
+! Set use_crop           by the compset you use and CLM_BLDNML_OPTS -crop setting
+! Set spinup_state       by the CLM_BLDNML_OPTS -bgc_spinup      setting
+! Set co2_ppmv           with CCSM_CO2_PPMV                      option
+! Set fatmlndfrc         with LND_DOMAIN_PATH/LND_DOMAIN_FILE    options
+! Set finidat            with RUN_REFCASE/RUN_REFDATE/RUN_REFTOD options for hybrid or branch cases
+!                        (includes $inst_string for multi-ensemble cases)
+!                        or with CLM_FORCE_COLDSTART to do a cold start
+!                        or set it with an explicit filename here.
+! Set maxpatch_glc       with GLC_NEC                            option
+! Set glc_do_dynglacier  with GLC_TWO_WAY_COUPLING               env variable
+!----------------------------------------------------------------------------------
+
+finidat = '/glade/u/home/sroos/scratch/restart_files/i.t520.IHistClm50BgcCrop.hcru_hcru_mt13.obsclim-histsoc-default-GSWP3-W5E5-go-1280.003_branch.clm2.r.1980-01-01-00000.nc'
+use_init_interp = .true.
+
+hist_empty_htapes = .true.
+
+hist_fincl1 = 'GRAINC_TO_FOOD', 'TSA', 'TV','ELAI', 'TLAI', 'TSOI','H2OSOI', 'QSOIL', 'QVEGT', 'TREFMNAV', 'SOILWATER_10CM', 'GPP', 'NPP'
+hist_fincl2 = 'ELAI','TVMAX', 'TVMIN', 'TV', 'TSA', 'H2OSOI', 'TSOI', 'TREFMXAV','TREFMNAV', 'SOILWATER_10CM', 'GPP', 'NPP', 'TVDAY', 'TVNIGHT', 'HS_NDAYS', 'HW', 'BGLFR', 'HSF', 'GRAINC_TO_FOOD'
+hist_fincl3 = 'GRAINC_TO_FOOD', 'LITFALL', 'ELAI','TLAI', 'TV', 'NPP', 'TVDAY', 'TVNIGHT'
+hist_fincl4 = 'ELAI','TLAI', 'TV','TVMAX', 'TVMIN', 'GPP', 'CPHASE', 'NPP', 'TVDAY', 'TVNIGHT','HS_NDAYS', 'HW', 'BGLFR', 'HSF', 'GRAINC_TO_FOOD'
+hist_fincl5 = 'GRAINC_TO_FOOD_ANN'
+
+hist_nhtfrq = 0, -24, 0, -24, 17520
+hist_mfilt  =1, 365, 1, 365, 999
+
+hist_type1d_pertape = '', '', 'PFTS', 'PFTS', 'PFTS'
+hist_dov2xy = .true., .true., .false., .false., .false.
+
+EOF
+
